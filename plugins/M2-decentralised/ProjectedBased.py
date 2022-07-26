2bec34311a76092e18675be2f595dfb9d6ded98f
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-07-26 13:00:58 +0200

    update projected based

--- plugins/M2-decentralised/ProjectedBased.py
+++ plugins/M2-decentralised/ProjectedBased.py
@@ -69,13 +69,10 @@ class ProjectedBased(ConflictDetection):
         
     def update(self, ownship, intruder):
         ''' Perform an update step of the Conflict Detection implementation. '''
-        self.confpairs, self.inconf, self.tcpamax, self.qdr, \
-            self.dist, self.dcpa, self.tcpa, self.tLOS, self.projected_lats, self.projected_lons, \
-                self.conftrks= self.detect(ownship, intruder, self.rpz, self.hpz, self.dtlookahead)
-                
-
-        # Check LOS the normal way
-        self.lospairs, self.qdr_mat, self.dist_mat = self.detect_los(ownship, intruder, self.rpz, self.hpz)
+        ''' Perform an update step of the Conflict Detection implementation. '''
+        self.confpairs, self.lospairs, self.inconf, self.tcpamax, self.qdr, \
+            self.dist, self.dcpa, self.tcpa, self.tLOS, self.qdr_mat, self.dist_mat = \
+                self.detect(ownship, intruder, self.rpz, self.hpz, self.dtlookahead)
 
         # confpairs has conflicts observed from both sides (a, b) and (b, a)
         # confpairs_unique keeps only one of these
@@ -88,8 +85,44 @@ class ProjectedBased(ConflictDetection):
         # Update confpairs_unique and lospairs_unique
         self.confpairs_unique = confpairs_unique
         self.lospairs_unique = lospairs_unique    
-        
+
     def detect(self, ownship, intruder, rpz, hpz, dtlookahead):
+
+        # run projected based
+        projected_based = self.detect_projectedbased(ownship, intruder, self.rpz, self.hpz, self.dtlookahead)
+
+        projected_confpairs = projected_based[0]        
+
+        # run statebased
+        statebased_confpairs, lospairs, inconf, tcpamax, qdr, \
+            dist, dcpa, tcpa, tLOS, qdr_mat, dist_mat = \
+                self.detect_statebased(ownship, intruder, rpz, hpz, dtlookahead)
+
+        # get indices from statebased that are in projected_basd
+        confpairs_indices = [statebased_confpairs.index(pair) for pair in statebased_confpairs
+                                                                        if pair in projected_confpairs]
+
+        # now only keep the confpairs that are in statebased
+        confpairs = [statebased_confpairs[i] for i in confpairs_indices]
+        tcpamax = [tcpamax[i] for i in confpairs_indices]
+        qdr = [qdr[i] for i in confpairs_indices]
+        dist = [dist[i] for i in confpairs_indices]
+        dcpa = [dcpa[i] for i in confpairs_indices]
+        tcpa = [tcpa[i] for i in confpairs_indices]
+        tLOS = [tLOS[i] for i in confpairs_indices]
+
+        # go through confpairs
+        conflict_ac = [bs.traf.id(acid) for acid in {acids[0] for acids in confpairs}]
+
+        inconf = np.array([False]*bs.traf.ntraf)
+
+        for idx in conflict_ac:
+            inconf[idx] = True
+
+        return confpairs, lospairs, inconf, tcpamax, \
+            qdr, dist, dcpa, tcpa, tLOS, qdr_mat, dist_mat
+        
+    def detect_projectedbased(self, ownship, intruder, rpz, hpz, dtlookahead):
         ''' Conflict detection between ownship (traf) and intruder (traf/adsb).'''
 
         ############### START PROJECTION ########################
@@ -647,11 +680,10 @@ class ProjectedBased(ConflictDetection):
         
         return merged_line
 
-    def detect_los(self, ownship, intruder, rpz, hpz):
+    def detect_statebased(self, ownship, intruder, rpz, hpz, dtlookahead):
         ''' Conflict detection between ownship (traf) and intruder (traf/adsb).'''
-
         # Calculate everything using the buffered RPZ
-        rpz = np.zeros(len(rpz)) + self.rpz_actual
+        rpz = np.zeros(len(rpz)) + self.rpz_buffered
         # Identity matrix of order ntraf: avoid ownship-ownship detected conflicts
         I = np.eye(ownship.ntraf)
 
@@ -663,7 +695,46 @@ class ProjectedBased(ConflictDetection):
 
         # Convert back to array to allow element-wise array multiplications later on
         # Convert to meters and add large value to own/own pairs
+        qdr = np.asarray(qdr)
         dist = np.asarray(dist) * nm + 1e9 * I
+
+        # Calculate horizontal closest point of approach (CPA)
+        qdrrad = np.radians(qdr)
+        dx = dist * np.sin(qdrrad)  # is pos j rel to i
+        dy = dist * np.cos(qdrrad)  # is pos j rel to i
+
+        # Ownship track angle and speed
+        owntrkrad = np.radians(ownship.trk)
+        ownu = ownship.gs * np.sin(owntrkrad).reshape((1, ownship.ntraf))  # m/s
+        ownv = ownship.gs * np.cos(owntrkrad).reshape((1, ownship.ntraf))  # m/s
+
+        # Intruder track angle and speed
+        inttrkrad = np.radians(intruder.trk)
+        intu = intruder.gs * np.sin(inttrkrad).reshape((1, ownship.ntraf))  # m/s
+        intv = intruder.gs * np.cos(inttrkrad).reshape((1, ownship.ntraf))  # m/s
+
+        du = ownu - intu.T  # Speed du[i,j] is perceived eastern speed of i to j
+        dv = ownv - intv.T  # Speed dv[i,j] is perceived northern speed of i to j
+
+        dv2 = du * du + dv * dv
+        dv2 = np.where(np.abs(dv2) < 1e-6, 1e-6, dv2)  # limit lower absolute value
+        vrel = np.sqrt(dv2)
+
+        tcpa = -(du * dx + dv * dy) / dv2 + 1e9 * I
+
+        # Calculate distance^2 at CPA (minimum distance^2)
+        dcpa2 = np.abs(dist * dist - tcpa * tcpa * dv2)
+
+        # Check for horizontal conflict
+        R2 = rpz * rpz
+        swhorconf = dcpa2 < R2  # conflict or not
+
+        # Calculate times of entering and leaving horizontal conflict
+        dxinhor = np.sqrt(np.maximum(0., R2 - dcpa2))  # half the distance travelled inzide zone
+        dtinhor = dxinhor / vrel
+
+        tinhor = np.where(swhorconf, tcpa - dtinhor, 1e8)  # Set very large if no conf
+        touthor = np.where(swhorconf, tcpa + dtinhor, -1e8)  # set very large if no conf
 
         # Vertical conflict --------------------------------------------------------
 
@@ -671,17 +742,39 @@ class ProjectedBased(ConflictDetection):
         dalt = ownship.alt.reshape((1, ownship.ntraf)) - \
             intruder.alt.reshape((1, ownship.ntraf)).T  + 1e9 * I
 
+        dvs = ownship.vs.reshape(1, ownship.ntraf) - \
+            intruder.vs.reshape(1, ownship.ntraf).T
+        dvs = np.where(np.abs(dvs) < 1e-6, 1e-6, dvs)  # prevent division by zero
+
+        # Check for passing through each others zone
+        tcrosshi = (dalt + hpz) / -dvs
+        tcrosslo = (dalt - hpz) / -dvs
+        tinver = np.minimum(tcrosshi, tcrosslo)
+        toutver = np.maximum(tcrosshi, tcrosslo)
+
+        # Combine vertical and horizontal conflict----------------------------------
+        tinconf = np.maximum(tinver, tinhor)
+        toutconf = np.minimum(toutver, touthor)
+
+        swconfl = np.array(swhorconf * (tinconf <= toutconf) * (toutconf > 0.0) * \
+            (tinconf < dtlookahead) * (1.0 - I), dtype=np.bool)
 
         # --------------------------------------------------------------------------
-        # Update LOS lists
+        # Update conflict lists
         # --------------------------------------------------------------------------
+        # Ownship conflict flag and max tCPA
+        inconf = np.any(swconfl, 1)
+        tcpamax = np.max(tcpa * swconfl, 1)
 
+        # Select conflicting pairs: each a/c gets their own record
+        confpairs = [(ownship.id[i], ownship.id[j]) for i, j in zip(*np.where(swconfl))]
         # It's a LOS if the actual RPZ of 32m is violated.
         swlos = (dist < (np.zeros(len(rpz)) + self.rpz_actual)) * (np.abs(dalt) < hpz)
         lospairs = [(ownship.id[i], ownship.id[j]) for i, j in zip(*np.where(swlos))]
 
-
-        return lospairs, qdr, dist
+        return confpairs, lospairs, inconf, tcpamax, \
+            qdr[swconfl], dist[swconfl], np.sqrt(dcpa2[swconfl]), \
+                tcpa[swconfl], tinconf[swconfl], qdr, dist
 
 
 
