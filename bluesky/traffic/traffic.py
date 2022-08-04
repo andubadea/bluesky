60da02189449fea845b24e2683bc7a0bcce81ee6
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-08-04 13:28:29 +0200

    update logging

--- bluesky/traffic/traffic.py
+++ bluesky/traffic/traffic.py
@@ -93,6 +93,10 @@ confheader = \
     'ALT2 [ft],' + \
     'CPALAT [lat],' + \
     'CPALON [lon],' + \
+    'AIRSPACETYPE1 [-],' + \
+    'AIRSPACETYPE2 [-],' + \
+    'LAYERTYE1 [-],' + \
+    'LAYERTYPE2 [-],' + \
     'EDGEID1 [-],' + \
     'EDGEID2 [-]\n'
     
@@ -113,7 +117,13 @@ losheader = \
     'LAT2 [deg],' + \
     'LON2 [deg],' + \
     'ALT2 [ft],' + \
-    'DIST [m]\n'
+    'DIST [m]' + \
+    'AIRSPACETYPE1 [-],' + \
+    'AIRSPACETYPE2 [-],' + \
+    'LAYERTYE1 [-],' + \
+    'LAYERTYPE2 [-],' + \
+    'EDGEID1 [-],' + \
+    'EDGEID2 [-]\n'
     
 regheader = \
     '#######################################################\n' + \
@@ -609,8 +619,10 @@ class Traffic(Entity):
                     self.conflog.log(pair[0], pair[1],
                                     self.lat[idx1], self.lon[idx1],self.alt[idx1],
                                     self.lat[idx2], self.lon[idx2],self.alt[idx2],
-                                    cpalatlon[0], cpalatlon[1], self.actedge.wpedgeid[idx1],
-                                    self.actedge.wpedgeid[idx2])
+                                    cpalatlon[0], cpalatlon[1],
+                                    self.actedge.edge_airspace_type[idx1], self.actedge.edge_airspace_type[idx2], 
+                                    self.flight_layer_type[idx1], self.flight_layer_type[idx2], 
+                                    self.actedge.wpedgeid[idx1],self.actedge.wpedgeid[idx2])
                 
         self.prevconfpairs = set(self.cd.confpairs)
         
@@ -635,8 +647,11 @@ class Traffic(Entity):
                     self.losmindist[dictkey] = [losdistance, 
                                                 self.lat[idx1], self.lon[idx1], self.alt[idx1], 
                                                 self.lat[idx2], self.lon[idx2], self.alt[idx2],
-                                                bs.sim.simt, bs.sim.simt]
-                    # The last guy over here                    ^ is the LOS start time
+                                                bs.sim.simt, bs.sim.simt,
+                    # This guy here                             ^ is the LOS start time
+                                                self.actedge.edge_airspace_type[idx1], self.actedge.edge_airspace_type[idx2], 
+                                                self.flight_layer_type[idx1], self.flight_layer_type[idx2],  
+                                                self.actedge.wpedgeid[idx1],self.actedge.wpedgeid[idx2]]
                 else:
                     # Entry exists, check if calculated is smaller
                     if self.losmindist[dictkey][0] > losdistance:
@@ -644,7 +659,10 @@ class Traffic(Entity):
                         self.losmindist[dictkey] = [losdistance, 
                                                 self.lat[idx1], self.lon[idx1], self.alt[idx1], 
                                                 self.lat[idx2], self.lon[idx2], self.alt[idx2],
-                                                bs.sim.simt, self.losmindist[dictkey][8]]
+                                                bs.sim.simt, self.losmindist[dictkey][8],
+                                                self.losmindist[dictkey][9], self.losmindist[dictkey][10],
+                                                self.losmindist[dictkey][11], self.losmindist[dictkey][12],
+                                                self.losmindist[dictkey][13], self.losmindist[dictkey][14]]
         
         # Log data if there are aircraft that are no longer in LOS
         if lospairs_out:
@@ -663,7 +681,8 @@ class Traffic(Entity):
                 self.loslog.log(losdata[8], losdata[7], pair[0], pair[1],
                                 losdata[1], losdata[2],losdata[3],
                                 losdata[4], losdata[5],losdata[6],
-                                losdata[0])
+                                losdata[0], losdata[9], losdata[10],
+                                losdata[11], losdata[12], losdata[13], losdata[14])
                 
         
         self.prevlospairs = set(self.cd.lospairs)
