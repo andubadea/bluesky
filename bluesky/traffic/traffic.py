f2b6be1a020d5ce2f44fbd8453b61c302828ae59
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2023-01-12 17:12:55 +0100

    add heading to loslog

--- bluesky/traffic/traffic.py
+++ bluesky/traffic/traffic.py
@@ -123,7 +123,9 @@ losheader = \
     'LAYERTYE1 [-],' + \
     'LAYERTYPE2 [-],' + \
     'EDGEID1 [-],' + \
-    'EDGEID2 [-]\n'
+    'EDGEID2 [-],' + \
+    'HDG1 [deg],' + \
+    'HDG2 [deg]\n'
 
 regheader = \
     '#######################################################\n' + \
@@ -675,7 +677,8 @@ class Traffic(Entity):
                     # This guy here                             ^ is the LOS start time
                                                 self.actedge.edge_airspace_type[idx1], self.actedge.edge_airspace_type[idx2], 
                                                 self.flight_layer_type[idx1], self.flight_layer_type[idx2],  
-                                                self.actedge.wpedgeid[idx1],self.actedge.wpedgeid[idx2]]
+                                                self.actedge.wpedgeid[idx1],self.actedge.wpedgeid[idx2],
+                                                self.hdg[idx1], self.hdg[idx2]]
                 else:
                     # Entry exists, check if calculated is smaller
                     if self.losmindist[dictkey][0] > losdistance:
@@ -686,7 +689,9 @@ class Traffic(Entity):
                                                 bs.sim.simt, self.losmindist[dictkey][8],
                                                 self.losmindist[dictkey][9], self.losmindist[dictkey][10],
                                                 self.losmindist[dictkey][11], self.losmindist[dictkey][12],
-                                                self.losmindist[dictkey][13], self.losmindist[dictkey][14]]
+                                                self.losmindist[dictkey][13], self.losmindist[dictkey][14],
+                                                self.losmindist[dictkey][15], self.losmindist[dictkey][16]
+                                                ]
         
         # Log data if there are aircraft that are no longer in LOS
         if lospairs_out:
@@ -706,7 +711,8 @@ class Traffic(Entity):
                                 losdata[1], losdata[2],losdata[3],
                                 losdata[4], losdata[5],losdata[6],
                                 losdata[0], losdata[9], losdata[10],
-                                losdata[11], losdata[12], losdata[13], losdata[14])
+                                losdata[11], losdata[12], losdata[13], losdata[14],
+                                losdata[15], losdata[16])
                 
         
         self.prevlospairs = set(self.cd.lospairs)
