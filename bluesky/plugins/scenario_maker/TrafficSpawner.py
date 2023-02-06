8766059062ee5d142dec23149364b4576cfa325f
andubadea <37139260+andubadea@users.noreply.github.com>
2023-02-06 13:17:21 +0100

    Fixed path issues.

--- bluesky/plugins/scenario_maker/TrafficSpawner.py
+++ bluesky/plugins/scenario_maker/TrafficSpawner.py
@@ -45,12 +45,12 @@ class TrafficSpawner(Entity):
     
     @command
     def loadcity(self, city = None):
-        list_of_cities = [x for x in os.listdir(f'plugins/scenario_maker/') if '.py' not in x]
+        list_of_cities = [x for x in os.listdir(f'bluesky/plugins/scenario_maker/') if '.py' not in x]
         if city == None or city not in list_of_cities:
             bs.scr.echo(f'The following cities are available: {list_of_cities}.')
             return
         self.city = city
-        self.path = f'plugins/scenario_maker/{self.city}'
+        self.path = f'bluesky/plugins/scenario_maker/{self.city}'
         self.load_origins_destinations()
         return
     
