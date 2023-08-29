7a96f895764082b648b3a424aee4ea9b3aa33ecb
andubadea <37139260+andubadea@users.noreply.github.com>
2023-08-29 16:42:30 +0200

    Added state-based CD and CR.

--- bluesky/plugins/detection_study/scenario_maker/TrafficSpawner.py
+++ bluesky/plugins/detection_study/scenario_maker/TrafficSpawner.py
@@ -130,15 +130,17 @@ class TrafficSpawner(Entity):
 
         G = ox.graph_from_gdfs(nodes, edges)
         
-        bs.stack.stack(f'SCHEDULE 00:00:00 PAN {self.city_centre_coords[0]},{self.city_centre_coords[1]}')
-        bs.stack.stack(f'SCHEDULE 00:00:00 ZOOM 15')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 PAN {self.city_centre_coords[0]},{self.city_centre_coords[1]}')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 ZOOM 15')
         # bs.stack.stack(f'SCHEDULE 00:00:01 CDMETHOD INTENTCD')
         # bs.stack.stack(f'SCHEDULE 00:00:01 RESO INTENTCR')
-        bs.stack.stack(f'SCHEDULE 00:00:00 CDMETHOD DEFENSIVECD')
-        bs.stack.stack(f'SCHEDULE 00:00:00 RESO DEFENSIVECR')
-        bs.stack.stack(f'SCHEDULE 00:00:00 STARTLOGS')
-        bs.stack.stack(f'SCHEDULE 00:00:00 STARTCDRLOGS')
-        bs.stack.stack(f'HOLD')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 CDMETHOD DEFENSIVECD')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 RESO DEFENSIVECR')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 CDMETHOD m22CD')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 RESO m22CR')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 STARTLOGS')
+        # bs.stack.stack(f'SCHEDULE 00:00:00 STARTCDRLOGS')
+        # bs.stack.stack(f'HOLD')
         return G, edges, nodes
     
     @command
