2cc426e18946e2d3d973f4147778264bb5fd0d48
andubadea <37139260+andubadea@users.noreply.github.com>
2023-09-12 11:54:17 +0200

    More scenario modifications.

--- bluesky/plugins/detection_study/scenario_maker/batch_maker.py
+++ bluesky/plugins/detection_study/scenario_maker/batch_maker.py
@@ -2,13 +2,15 @@
 
 batchfilename = 'scenario/CDR/batch.scn'
 # How many repetitions
-num_repetitions = 10
+num_repetitions = 5
 
 scenario_names = []
 
 # Independent variables
 #densities = [50, 100, 150, 200, 250]
 densities = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
+winds = [2,4,6,8]
+directions = [0, 90, 180, 270]
 cd_cr_methods = [['M22CD', 'M22CR'],['INTENTCD', 'DEFENSIVECR'], ['DEFENSIVECD', 'DEFENSIVECR']]
 
 for density in densities:
@@ -20,8 +22,9 @@ for density in densities:
                     f'00:00:00>RESO {method[1]}\n' + \
                     '00:00:00>STARTLOGS\n' + \
                     '00:00:00>STARTCDRLOGS\n' + \
+                    '00:00:00>SETWIND 0 0\n' + \
                     '00:00:01>FF'
-            scen_name = f'CDR_{method[0]}_{density}_{i}_CR'
+            scen_name = f'CDR_{method[0]}_{density}_{i}_{0}_{0}_CR'
             scenario_names.append(scen_name)
             with open(f'scenario/CDR/{scen_name}.scn', 'w') as f:
                 f.write(to_write)
@@ -34,11 +37,30 @@ for density in densities:
                     f'00:00:00>CDMETHOD {method[0]}\n' + \
                     '00:00:00>STARTLOGS\n' + \
                     '00:00:00>STARTCDRLOGS\n' + \
+                    '00:00:00>SETWIND 0 0\n' + \
                     '00:00:01>FF'
-            scen_name = f'CDR_{method[0]}_{density}_{i}_NR'
+            scen_name = f'CDR_{method[0]}_{density}_{i}_{0}_{0}_NR'
             scenario_names.append(scen_name)
             with open(f'scenario/CDR/{scen_name}.scn', 'w') as f:
                 f.write(to_write)
+                
+# Wind
+for windmag in winds:
+    for winddir in directions:
+        for method in cd_cr_methods:
+            for i in range(num_repetitions):
+                density = 300
+                to_write = f'00:00:00>TRAFFICNUMBER {density}\n' + \
+                        f'00:00:00>SEED {i}\n' + \
+                        f'00:00:00>CDMETHOD {method[0]}\n' + \
+                        '00:00:00>STARTLOGS\n' + \
+                        '00:00:00>STARTCDRLOGS\n' + \
+                        f'00:00:00>SETWIND {windmag} {winddir}\n' + \
+                        '00:00:01>FF'
+                scen_name = f'CDR_{method[0]}_{density}_{windmag}_{winddir}_{i}_CR'
+                scenario_names.append(scen_name)
+                with open(f'scenario/CDR/{scen_name}.scn', 'w') as f:
+                    f.write(to_write)
 
 with open(batchfilename, 'w') as f:
     for name in scenario_names:
@@ -46,5 +68,4 @@ with open(batchfilename, 'w') as f:
                     f'00:00:00.00>PCALL CDR/{name}.scn\n' + \
                     '00:00:00>SCHEDULE 02:00:00 HOLD\n' + \
                     '00:00:00.00>FF\n\n'
-                    
         f.write(to_write)
