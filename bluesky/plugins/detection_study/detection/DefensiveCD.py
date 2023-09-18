4f1ee3b8247b8ff6cdc6ac5e119984dcf57a6030
andubadea <37139260+andubadea@users.noreply.github.com>
2023-09-18 11:50:59 +0200

    Lookahead time and update study

--- bluesky/plugins/detection_study/detection/DefensiveCD.py
+++ bluesky/plugins/detection_study/detection/DefensiveCD.py
@@ -63,7 +63,7 @@ class DefensiveCD(ConflictDetection):
         super().__init__()
         # Lookahead parameters
         self.lookahead_min = 100 #metres
-        self.lookahead_max = 300 #metres
+        self.lookahead_max = 500 #metres
         
         # New detection parameters
         self.intent_geom = [] # Linestring of aircraft intent per pair
